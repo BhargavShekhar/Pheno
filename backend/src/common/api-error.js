fe837fff9616a532f2a2e5226261d7c43fda3ec9
Nenikanot0 <meherneharika503@gmail.com>
2026-09-15 14:08:57 +0530

    added api error

--- backend/src/common/api-error.js
+++ backend/src/common/api-error.js
@@ -1,34 +1,45 @@
-export class ApiError extends Error {
-    constructor(message="internal server error", statusCode=500) {
+class ApiError extends Error {
+    constructor (message="Internal Server Error",statusCode=500) {
         super(message);
 
         this.statusCode = statusCode;
-        this.name = this.constructor.message;
 
-        Error.captureStackTrace(this, this.constructor);
+        this.name = this.constructor.name;
+
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, this.constructor);
+        }
     }
 
-    static badRequest(message="bad request") {
+    static badRequest(message="bad request"){
         return new ApiError(message, 400);
     }
 
-    static unauthorized(message="Unauthorize") {
+    static unauthorized(message="unauthorized"){
         return new ApiError(message, 401);
     }
 
-    static conflict(message="Conflict - data already exists") {
-        return new ApiError(message, 409);
-    }
-
-    static forbidden(message="forbidden") {
+    static forbidden(message="forbidden"){
         return new ApiError(message, 403);
     }
 
-    static notfound(message="notfound") {
+    static notFound(message="not found") {
         return new ApiError(message, 404);
     }
-
-    static internal(message="internal server error") {
+    
+    static internalServerError(message="internal server error"){
         return new ApiError(message, 500);
+    }
+
+    static badGateway(message="bad gateway"){
+        return new ApiError(message,502);
+    }
+
+    static serviceUnavailable(message="service unavailable"){
+        return new ApiError(message, 503);
+    }
+
+    static gatewayTimeOut(message="gateway timeout"){
+        return new ApiError(message, 504);
     }
 }
