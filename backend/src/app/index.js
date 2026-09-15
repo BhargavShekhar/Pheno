731394eaee9d79f2d12983087f5fb1ee6f53ac89
Bhargav Shekar <bhargavpathak91021@gmail.com>
2026-09-15 16:13:23 +0530

    added auto formating

--- backend/src/app/index.js
+++ backend/src/app/index.js
@@ -7,17 +7,20 @@ export function createExpressApp() {
     app.use(express.json());
     app.use(cors());
 
-    app.get("/health", (req, res => res.json({ healthy: true })));
+    app.get("/health", ((req, res) => res.json({ healthy: true })));
 
     app.use((error, req, res, next) => {
-        console.log(error);
+        console.log(error.stack);
+
+        const statusCode = err.statusCode || 500;
 
         res.status(500).json({
             sucess: false,
             message: { error: error.message },
             data: null
         })
+
     })
 
     return app;
-}
+}x
